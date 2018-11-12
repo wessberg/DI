ba4c880585162f05b65b15dcb5421db6c7c2c922
wessberg <frederikwessberg@gmail.com>
2018-11-12 20:49:42 +0100

    Disabled lint in a specific file

--- src/implementation/implementation.ts
+++ src/implementation/implementation.ts
@@ -2,5 +2,7 @@ import {NewableService} from "../newable-service/newable-service";
 import {CustomConstructableService} from "../custom-constructable-service/custom-constructable-service";
 import {IWithConstructorArgumentsSymbol} from "../constructor-arguments/constructor-argument";
 
+// tslint:disable
+
 export type Implementation<T> = NewableService<T>&IWithConstructorArgumentsSymbol;
 export type ImplementationInstance<T> = CustomConstructableService<T>&IWithConstructorArgumentsSymbol;
