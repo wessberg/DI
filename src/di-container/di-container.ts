d6760fb2a3748a6861c1c40790201cec306b3ee9
Frederik Wessberg <frederikwessberg@gmail.com>
2022-05-25 20:32:51 +0200

    feat: add new sponsor: scrubtheweb. Run Prettier

--- src/di-container/di-container.ts
+++ src/di-container/di-container.ts
@@ -326,4 +326,4 @@ export class DIContainer implements IDIContainer {
   }
 }
 
-const DI_COMPILER_ERROR_HINT = `Note: You must use DI-Compiler (https://github.com/wessberg/di-compiler) for this library to work correctly. Please consult the readme for instructions on how to install and configure it for your project.`
+const DI_COMPILER_ERROR_HINT = `Note: You must use DI-Compiler (https://github.com/wessberg/di-compiler) for this library to work correctly. Please consult the readme for instructions on how to install and configure it for your project.`;
